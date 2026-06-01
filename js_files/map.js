55fe49d41051940d1a36960a1b30589d80c2bd42
Lev Zurcher <lazurcher5@gmail.com>
2026-06-01 14:24:36 -0600

    Add div item_details

--- js_files/map.js
+++ js_files/map.js
@@ -1,4 +1,5 @@
 var map = L.map('map').setView([43.819046, -111.783228], 14);
+var itemDetails = document.getElementById('item_details');
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -15,6 +16,8 @@ function onMapClick(e) {
         .setLatLng(e.latlng)
         .setContent("You clicked the map at " + e.latlng.toString())
         .openOn(map);
+
+    itemDetails.innerHTML = "<p>You clicked the map at " + e.latlng.toString() + "</p>";
 }
 
 map.on('click', onMapClick);
